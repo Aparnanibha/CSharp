19450a1d301e213814f5e8d42e27992acc42b932
Aparna training <aparna.b.aparna@capgemini.com>
2022-02-18 17:54:36 +0530

    Commit

---
+++ ProgramSolve.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello
+{
+    class ProgramSolve
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hahhahaha i am doing bit good");
+        }
+    }
+}
